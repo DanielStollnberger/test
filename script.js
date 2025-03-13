a87bfd0b132752d6ee389623d08d2bf1661516a7
Daniel Stollnberger <daniiistolliii@gmail.com>
2025-03-13 15:54:10 +0100

    tried some stuff

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ let myObjects = {
         'company': 'voest',
         'work': 'proofer'
     },
+    'logJob': function () {
+        return 'Daniel' +' '+ myObjects.job.work+ ' at '+myObjects.job.company;
+    }
 };
 
-console.log(myObjects);
+
+
+function addPage() {
+let contentRef = document.getElementById('content');
+    contentRef.innerHTML= showFullJob();
+    document.getElementById('content').innerHTML = myObjects.logJob();
+}
+
+function showFullJob() {
+    return myObjects.job.company +' '+ myObjects.job.work +' '+myObjects.age;
+}
